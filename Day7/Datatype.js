efd4f9582b3c68968e101af16e99a579adb99ec0
AnkitaChaudhary-09 <ankitachaudhary1009@gmail.com>
2025-06-09 18:15:58 +0530

    update javaScript

---
+++ Day7/Datatype.js
@@ -0,0 +1,34 @@
+//Boolean
+console.log(5>8);
+console.log(5<8);
+
+//String
+console.log('true')
+console.log('Ankita')
+
+// Interger
+let age = 20;
+console.log("Age:", age);
+
+// Null
+let Class = null;
+console.log("Class:", Class);
+
+// Undefined
+let year;
+console.log("year:", year);
+
+// var
+var city = "Agra";
+console.log("City (var):", city);
+// you can update, it's value and access from everywhere.
+
+// let
+let state = "UP";
+console.log("State:", state);
+//you cann't update,it's value.
+
+// const
+const country = "India";
+console.log("Country:", country);
+//you cann't update,it's value.
