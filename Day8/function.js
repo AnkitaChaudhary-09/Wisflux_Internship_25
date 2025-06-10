bea9ec892e2c5beef82210d9057301c424b9d5e3
AnkitaChaudhary-09 <ankitachaudhary1009@gmail.com>
2025-06-10 19:53:04 +0530

    update javaScript

---
+++ Day8/function.js
@@ -0,0 +1,20 @@
+//functions
+function prinAge(){
+   console.log(`your age is ${age}`);
+    
+}
+
+age=20;
+prinAge();
+// objects
+ let product={
+   company: 'Mango',
+   name: 'cotton T-shirt',
+   price:'530',
+
+ };
+ console.log(product);
+ console.log(product.company);
+ console.log(product.price);
+ console.log(product.name);
+ 
