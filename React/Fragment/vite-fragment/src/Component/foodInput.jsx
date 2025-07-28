8e6ade9a173c20656aca499f60db981d9ff5718c
AnkitaChaudhary-09 <ankitachaudhary1009@gmail.com>
2025-07-28 15:39:49 +0530

    UseState

--- React/Fragment/vite-fragment/src/Component/foodInput.jsx
+++ React/Fragment/vite-fragment/src/Component/foodInput.jsx
@@ -1,10 +1,12 @@
-import styles from './foodInput.module.css';
-const FoodInput=()=>{
-  const handleChange = (event) => {
-    console.log(event.target.value);
-  };
-return <input type='text' placeholder='Enter Food Item here' className={styles.FoodInput}
-onChange={handleChange}/>
-
-}
+import styles from "./foodInput.module.css";
+const FoodInput = ({ handleChange }) => {
+  return (
+    <input
+      type="text"
+      placeholder="Enter Food Item here"
+      className={styles.FoodInput}
+      onKeyDown={handleChange}
+    />
+  );
+};
 export default FoodInput;
